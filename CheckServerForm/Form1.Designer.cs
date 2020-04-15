c82e91a06a132846892460e0e42394eb81997549
祐祐 └ <coolkerry2003@hotmail.com.tw>
2020-04-15 13:47:34 +0800

    加入專案檔案。

---
+++ CheckServerForm/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace CheckServerForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(193, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("標楷體", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(175, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "檢測Server是否啟動...";
+            // 
+            // listBox
+            // 
+            this.listBox.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.listBox.FormattingEnabled = true;
+            this.listBox.ItemHeight = 16;
+            this.listBox.Location = new System.Drawing.Point(12, 27);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(388, 68);
+            this.listBox.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(412, 106);
+            this.Controls.Add(this.listBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "CheckServerWorking";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox;
+    }
+}
+
---
+++ CheckServerForm/Form1.cs
@@ -0,0 +1,78 @@
+﻿using PilotGaea.Serialize;
+using PilotGaea.TMPClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CheckServerForm
+{
+    public partial class Form1 : Form
+    {
+        int second = 0;
+        int secondIterval = 60;
+        string url = "http://127.0.0.1:8080/docmd?cmd=";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (!CheckIsAlive()) StartServer();
+
+            second++;
+            label1.Text = second.ToString();
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            if (!CheckIsAlive()) StartServer();
+            
+            timer1.Start();
+            timer1.Interval = secondIterval * 1000;
+        }
+
+        private bool CheckIsAlive()
+        {
+            CMapDocument cmDoc = new CMapDocument();
+            VarStruct inputnum = new VarStruct();
+            VarStruct outputnum = new VarStruct();
+
+            bool connect = false;
+            string doCmdUrl = url + "isLive";
+            connect = cmDoc.DoCommand(doCmdUrl, ref inputnum, ref outputnum);
+
+            return connect;
+        }
+        private void StartServer()
+        {
+            //[Server異常]
+            //WerFault.exe 以及PGMapServer.exe
+            string PrcocessName = "PGMapServer";
+            string target = @"C:\Program Files\PilotGaea\TileMap\PGMapServer.exe";
+            Process[] MyProcess = Process.GetProcessesByName(PrcocessName);
+            if (MyProcess.Length > 0)
+            {
+                MyProcess[0].Kill(); //關閉執行中的程式
+                MyProcess[0].WaitForExit();
+            }
+            string SystemErrorName = "WerFault";
+            Process[] SystemErrorProcess = Process.GetProcessesByName(SystemErrorName);
+            if (SystemErrorProcess.Length > 0)
+            {
+                SystemErrorProcess[0].Kill(); //關閉執行中的程式
+                SystemErrorProcess[0].WaitForExit();
+            }
+            Process.Start(target, "-s 1");
+
+            string str = "重啟伺服器。In TimerCallback: " + DateTime.Now;
+            listBox.Items.Add(str);
+        }
+    }
+}
